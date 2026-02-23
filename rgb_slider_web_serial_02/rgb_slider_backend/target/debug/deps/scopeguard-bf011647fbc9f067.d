e6cc9b54d0cb14ecfa55ddcca1d3338e225aff20
MiguelCigano <miguel_m0130@hotmail.com>
2026-02-22 21:57:11 -0800

    clean: delete target dir 02

--- rgb_slider_web_serial_02/rgb_slider_backend/target/debug/deps/scopeguard-bf011647fbc9f067.d
+++
@@ -1,5 +0,0 @@
-/home/jm-liberty/Documents/thunder_rust/rgb_slider_web_serial_01/rgb_slider_backend/target/debug/deps/scopeguard-bf011647fbc9f067.d: /home/jm-liberty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/jm-liberty/Documents/thunder_rust/rgb_slider_web_serial_01/rgb_slider_backend/target/debug/deps/libscopeguard-bf011647fbc9f067.rmeta: /home/jm-liberty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/jm-liberty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs:
--- rgb_slider_web_serial_02/rgb_slider_backend/target/debug/deps/smallvec-26686e1fe6be83fa.d
+++
@@ -1,5 +0,0 @@
-/home/jm-liberty/Documents/thunder_rust/rgb_slider_web_serial_01/rgb_slider_backend/target/debug/deps/smallvec-26686e1fe6be83fa.d: /home/jm-liberty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
-
-/home/jm-liberty/Documents/thunder_rust/rgb_slider_web_serial_01/rgb_slider_backend/target/debug/deps/libsmallvec-26686e1fe6be83fa.rmeta: /home/jm-liberty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
-
-/home/jm-liberty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs:
